b3f9e49a963afcaf2cc1958f422c849ab039ef9e
Aykut Gedik <aykut@agentiq.com>
2018-02-09 12:31:55 -0800

    custom validator

--- README.md
+++ README.md
@@ -1,86 +1,5 @@
-# Swagger-oneOf-validator
+### Custom Validator
 
-"oneOf" verifies specific schema types (Message Type). This is a custom validator to accept custom "message_type" validation. (see example below)  
-
-```
-# swagger2 does not accept this
-oneOf:
-  - $ref: '#/components/schemas/Cat'
-  - $ref: '#/components/schemas/Dog'
-```
-
-** oneOf is currently only support in swagger3, swagger3 is missing code generator
-** https://swagger.io/docs/specification/data-models/oneof-anyof-allof-not/
-
-### Installation:
-Using npm:
-```sh
-npm install swagger-oneOf-validator --save
-```
-Using yarn:
-```sh
-yarn add swagger-oneOf-validator
-```
-
-### Usage:
-
-First create definitions in swagger.yaml
-```yaml
-definitions:
-  TextMessage:
-    type: object
-    required:
-      - content
-    properties:
-      content:
-        type: string
-  AseetMessage:
-    type: object
-    properties:
-      message_type:
-        type: string
-        enum:
-          - asset
-      asset:
-        type: object
-        properties:
-          asset_id:
-            type: integer
-        required:
-          - asset_id
-    required:
-      - asset
-      - message_type
-
-  RichTextMessage:
-    type: object
-    properties:
-      message_type:
-        type: string
-        enum:
-          - rich_text
-      rich_text:
-        type: object
-        properties:
-          content:
-            type: string
-          attachments:
-            type: array
-            items:
-              type: object
-            minItems: 1
-        required:
-          - content
-    required:
-      - rich_text
-      - message_type
-```
-
-Second inport the validator and create Validator instance with your swagger.yaml path
-Message definition mapping will be used to expect as oneOf in payload
-and can be also nested by defining nesting parameter name in validator.validateOneOf function
-result = validator.validateOneOf(definitionMapping, data, nestParameterName)
-if there is errors, it will be in result object.
 ```js
 
 const Validator = require('swagger-oneof-validator');
@@ -88,28 +7,7 @@ const path = require('path');
 const swaggerFilePath = path.join(__dirname, '/../../../public/swagger.yaml');
 const validator = new Validator(swaggerFilePath);
 
-// mapping message_type to definitions in swagger yaml
-// if there is not message_type found in structure, default definition will be used!
-const messageDefinitionMapping = {
-    'default': 'TextMessage',
-    'rich_text': 'RichTextMessage',
-    'asset': 'AseetMessage',
-    'reply': 'ReplyMessage',
-    'card': 'CardMessage'
-};
 
-router.post('/', (req, res, next) => {
-    const result = validator.validateOneOf(
-        messageDefinitionMapping,
-        req.body,
-        'attachments'
-    );
-    if (result.errors.length > 0) {
-        next(result.errors);
-    } else {
-        res.json('Valid');
-    }
-});
 
 ```
 
--- index.js
+++ index.js
@@ -12,9 +12,9 @@ class MessageValidator {
         let swaggerApi;
         sway.create(options).then(api => {
             swaggerApi = api;
-        }).catch(err => {
+    }).catch(err => {
             throw err;
-        });
+    });
         while (swaggerApi === undefined) {
             require('deasync').sleep(100);
         }
@@ -33,35 +33,54 @@ class MessageValidator {
             let i = 0;
             data[messageType][nestParameterName].forEach(item => {
                 let mergedPath = path.concat(tempPath);
-                mergedPath.push(nestParameterName+'['+i+']');
-                this._parseNestedMessages(item, messages, messageTypes, nestParameterName, mergedPath);
-                i++;
-            });
+            mergedPath.push(nestParameterName+'['+i+']');
+            this._parseNestedMessages(item, messages, messageTypes, nestParameterName, mergedPath);
+            i++;
+        });
+        }
+    }
+    _parseCustomMessages(data, messages, nestParameterName, path=null) {
+        if (!path) {
+            path = [];
+        }
+        messages.push({message: data, path});
+        let tempPath = [];
+        const messageType = data.message_type;
+        if (messageType && nestParameterName
+            && data[messageType] && data[messageType][nestParameterName]) {
+            tempPath.push(messageType);
+            let i = 0;
+            data[messageType][nestParameterName].forEach(item => {
+                let mergedPath = path.concat(tempPath);
+            mergedPath.push(nestParameterName+'['+i+']');
+            this._parseCustomMessages(item, messages, nestParameterName, mergedPath);
+            i++;
+        });
         }
     }
     _validateMessagesArray(messages, definitionMapping) {
         const errors = [];
         messages.forEach(data => {
             const {message, path} = data;
-            const messageType = message.message_type;
-            const definitionName = messageType ? definitionMapping[messageType] : definitionMapping['default'];
-            if (definitionName) {
-                const result = this.validateByDefinition(definitionName, message);
-                if (result.errors.length > 0) {
-                    const formatError = result.errors.map(item => {
-                        item.path = path.concat(item.path);
-                        return item;
-                    });
-                    formatError.map(item => errors.push(item));
-                }
-            } else {
-                errors.push({
-                    code: 'INVALID_MESSAGE_TYPE',
-                    message: "Message type: '" + messageType + "' is invalid",
-                    path: path
-                });
+        const messageType = message.message_type;
+        const definitionName = messageType ? definitionMapping[messageType] : definitionMapping['default'];
+        if (definitionName) {
+            const result = this.validateByDefinition(definitionName, message);
+            if (result.errors.length > 0) {
+                const formatError = result.errors.map(item => {
+                    item.path = path.concat(item.path);
+                return item;
+            });
+                formatError.map(item => errors.push(item));
             }
-        });
+        } else {
+            errors.push({
+                code: 'INVALID_MESSAGE_TYPE',
+                message: "Message type: '" + messageType + "' is invalid",
+                path: path
+            });
+        }
+    });
         return {errors};
     }
     validateByPath(req, path=null, method=null) {
@@ -100,11 +119,61 @@ class MessageValidator {
             path: []
         }]};;
     }
+    validateByDefinitionBySchema(data, schema) {
+        let result;
+        if (this.swaggerApi.definitions && schema) {
+            result = helpers.validateAgainstSchema(
+                helpers.getJSONSchemaValidator(),
+                schema,
+                data
+            );
+            return result;
+        }
+        return {errors: [{
+            code: 'INVALID_DEFINITION_ERROR',
+            message: "Definition is invalid.",
+            path: []
+        }]};;
+    }
     validateOneOf(definitionMapping, data, nestParameterName=null) {
         const messageTypes = Object.keys(definitionMapping);
         let parsedMessages = [];
         this._parseNestedMessages(data, parsedMessages, messageTypes, nestParameterName);
         return this._validateMessagesArray(parsedMessages, definitionMapping);
+    }
+    customMessageValidator(data, messageSchema, nestParameterName) {
+        let parsedMessages = [];
+        this._parseCustomMessages(data, parsedMessages, nestParameterName);
+        const errors = [];
+        parsedMessages.forEach(data => {
+            const {message, path} = data;
+        let errorMessage = 'Message format is invalid. Reasons: Missing content,';
+        errorMessage += ' missing (message_type or payload[message_type]) or having both together';
+        const error = {
+            code: 'INVALID_MESSAGE_FORMAT',
+            message: errorMessage,
+            path: path
+        }
+        const messageType = message.message_type;
+        // check that either content or (messageType && message[messageType]) not exists
+        // or all of them exists
+        // it has to has only one type of message
+        if (!message['content'] && !(messageType && message[messageType])) {
+            errors.push(error);
+        } else if(message['content'] && messageType && message[messageType]) {
+            errors.push(error);
+        } else {
+            const result = this.validateByDefinitionBySchema(message, messageSchema);
+            if (result.errors.length > 0) {
+                const formatError = result.errors.map(item => {
+                    item.path = path.concat(item.path);
+                return item;
+            });
+                formatError.map(item => errors.push(item));
+            }
+        }
+    });
+        return {errors};
     }
     middleware(path, method) {
         return (req, res, next) => {
